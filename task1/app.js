e6a86ca2c1ca95a519a5494d0103edcf649e2be5
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2024-02-06 12:15:56 +0100

    feat(menu): implement improved dropdown behavior

--- task1/app.js
+++ task1/app.js
@@ -1,25 +1,36 @@
-//
-
+//dropdown
 document.addEventListener("DOMContentLoaded", function () {
   let dropdowns = document.querySelectorAll(".dropdown-btn");
 
   dropdowns.forEach(function (dropdown) {
     dropdown.addEventListener("click", function () {
       let dropdownContent = this.nextElementSibling;
-      let icon = this.querySelector("img");
+
+      closeOtherDropdowns(this);
 
       if (dropdownContent.style.display === "flex") {
         dropdownContent.style.display = "none";
-        icon.src = "./assets/icon_down.png";
         dropdown.classList.remove("up");
       } else {
         dropdownContent.style.display = "flex";
-        icon.src = "./assets/icon_up.png";
         dropdown.classList.add("up");
       }
     });
   });
 });
+
+function closeOtherDropdowns(clickedDropdown) {
+  let allDropdowns = document.querySelectorAll(".dropdown-btn");
+
+  allDropdowns.forEach(function (dropdown) {
+    if (dropdown !== clickedDropdown) {
+      let dropdownContent = dropdown.nextElementSibling;
+
+      dropdownContent.style.display = "none";
+      dropdown.classList.remove("up");
+    }
+  });
+}
 
 //mobile-menu
 function toggleNavList() {
